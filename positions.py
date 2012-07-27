799e9f29f3e2d92c20f3e23349efe9a64ed853ad
Lee Walsh <lawalsh@physics.umass.edu>
2012-07-27 02:26:43 -0400

    positions.py now tracks statid id for each particle

--- positions.py
+++ positions.py
@@ -2,34 +2,69 @@ import matplotlib.pyplot as pl
 import numpy as np
 from PIL import Image
 
-bimage = Image.open('n20_bw_dots/n20_b_w_dots_0010.tif')
+bgimage = Image.open('n20_bw_dots/n20_b_w_dots_0010.tif') # for bkground in plot
 datapath = 'n20_bw_dots/Results-all.txt'
 
 data = [ line[:-1] for line in open(datapath)] # last char in each line is newline
 datatypes = data[0].split()#('/t') # split with no arg figures it out
-data = [ dataline.split() for dataline in data[1:] ]    # remove first line, split each column
+data = [ dataline.split() for dataline in data[1:50] ]    # remove first line, split each column
+for dot in data:
+    dot.append(0.0)
+data = np.array(data).astype(float)
 
-ndots = 10
-nimgs = len(data) / ndots
-
-dots = [ [ dotline for dotline in data[i*ndots:(i+1)*ndots] ] for i in range(nimgs) ]
 
 # indices in file (number gives column)
-ii = 0
-isize = 1
-ix = 2
-iy = 3
-islice = 4
+iid = 0     # unique particle id
+iarea = 1   # particle 
+ix = 2      # x position
+iy = 3      # y position
+islice = 4  # slice (image) number
+isid = 5    # static id (tracks particles)
 
-xs = [ [ dot[ix] for dot in img] for img in dots]
-ys = [ [ dot[iy] for dot in img] for img in dots]
-ys = [ [ 600-float(yn) for yn in yi ] for yi in ys]
+## Assuming all particles will be found in each frame
+#ndots = 10 
+#nimgs = len(data) / ndots
+#
+#dots = [ [ dotline for dotline in data[i*ndots:(i+1)*ndots] ] for i in range(nimgs) ]
 
-for slice in 
+#xs = [ [ dot[ix] for dot in img] for img in dots]
+#ys = [ [ dot[iy] for dot in img] for img in dots]
+#ys = [ [ 600-float(yn) for yn in yi ] for yi in ys]
 
-pl.plot(xs,ys,',')
-pl.imshow(bimage,origin='lower')
+#for slice in 
+
+#pl.plot(xs,ys,',')
+#pl.imshow(bgimage,origin='lower')
 #pl.axes().set_aspect(1)
-pl.show()
+#pl.show()
+
+nslice = int(data[len(data)-1][islice])
+
+for slice in range(nslice):
+    prev = np.nonzero(data[:,islice]==slice)
+    curr = np.nonzero(data[:,islice]==slice+1)
+
+    # for first image, use original particle ID as statid ID
+    if slice == 0:
+        data[curr,isid] = data[curr,iid]
+    else:
+        for newdot in data[curr]:
+            dist=1000.
+            for olddot in data[prev]:
+                newdist = (newdot[ix]-olddot[ix])**2 + (newdot[iy]-olddot[iy])**2
+                if newdist < dist:
+                    dist = newdist 
+                    #print "Closer"
+                    #print "new",newdot
+                    #print "old",olddot
+                    #newdot[isid] = olddot[isid] ### why doesn't this update data????
+                    data[newdot[iid]-1,isid] = olddot[isid] #must do this; previous line doesn't work
+                    #print "Now:", newdot
+                    #print "data:"
+                    #print data[curr]
+
+
+
+
 
 
