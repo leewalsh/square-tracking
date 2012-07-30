a5fe15d34173ebd226237ca5ea8b2d72dba62af0
Lee Walsh <lawalsh@physics.umass.edu>
2012-07-30 15:22:11 -0400

    positions.py now computes and plots mean squared displacement

--- positions.py
+++ positions.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as pl
+import matplotlib.cm as cm
 import numpy as np
+from scipy.stats import nanmean
 from PIL import Image
 
 bgimage = Image.open('n20_bw_dots/n20_b_w_dots_0010.tif') # for bkground in plot
@@ -7,8 +9,9 @@ datapath = 'n20_bw_dots/Results-all.txt'
 
 data = [ line[:-1] for line in open(datapath)] # last char in each line is newline
 datatypes = data[0].split()#('/t') # split with no arg figures it out
-data = [ dataline.split() for dataline in data[1:50] ]    # remove first line, split each column
+data = [ dataline.split() for dataline in data[1:] ]    # remove first line, split each column
 for dot in data:
+    dot.append(0.0)
     dot.append(0.0)
 data = np.array(data).astype(float)
 
@@ -20,12 +23,63 @@ ix = 2      # x position
 iy = 3      # y position
 islice = 4  # slice (image) number
 isid = 5    # static id (tracks particles)
+idisp = 6   # particle displacement from initial position
 
 ## Assuming all particles will be found in each frame
 #ndots = 10 
 #nimgs = len(data) / ndots
 #
 #dots = [ [ dotline for dotline in data[i*ndots:(i+1)*ndots] ] for i in range(nimgs) ]
+
+
+nslice = int(data[len(data)-1][islice])
+msqdisp = np.zeros(nslice)
+dists = []
+for slice in range(nslice):
+    prevprev = np.nonzero(data[:,islice]==slice-1)
+    prev = np.nonzero(data[:,islice]==slice)
+    curr = np.nonzero(data[:,islice]==slice+1)
+
+    # for first image, use original particle ID as statid ID
+    if slice == 0:
+        firs = curr
+        data[firs,isid] = data[firs,iid]
+        #data[curr,idisp] = 0.0
+        initx = data[firs,ix]
+        inity = data[firs,iy]
+    else:
+        for newdot in data[curr]:
+            dist=12.
+            for olddot in data[prev]:
+                newdist = (newdot[ix]-olddot[ix])**2 + (newdot[iy]-olddot[iy])**2
+                if newdist < dist:
+                    dist = newdist 
+                    #print "Closer"
+                    #print "new",newdot," old",olddot
+                    #newdot[isid] = olddot[isid] ### why doesn't this update data????
+                    newsid = olddot[isid]
+                    data[newdot[iid]-1,isid] = newsid #must do this; previous line doesn't work
+                    #print "Now:", newdot
+                    #print "data: ", data[curr]
+            dists.append(dist)
+            if dist>11.9:
+                #print 'slice ',slice,' missing ',newdot[iid]
+                newsid=0
+                data[newdot[iid]-1,isid] = newsid
+            #sqdisp = (newdot[ix] - olddot[ix])**2 + (newdot[iy]-olddot[iy])**2
+            #print newsid
+            sqdisp = (newdot[ix] - data[np.nonzero(data[:,iid]==newsid),ix])**2 \
+                    + (newdot[iy] - data[np.nonzero(data[:,iid]==newsid),iy])**2
+            #sqdisp = (newdot[ix] - data[newsid-1,ix])**2 \
+            #       + (newdot[iy] - data[newsid-1,iy])**2
+            data[newdot[iid]-1,idisp] = float(sqdisp) if bool(newsid) & (np.shape(sqdisp)==(1,1)) else None
+            #print type(sqdisp),np.shape(sqdisp),sqdisp
+        msqdisp[slice] = nanmean(data[np.nonzero(data[:,islice]==slice+1),idisp][0])
+        #print slice, msqdisp[slice],data[np.nonzero(data[:,islice]==slice+1),idisp],data[np.nonzero(data[:,islice]==slice+1),isid]
+        #if slice==6: break
+
+
+# Plot them:
 
 #xs = [ [ dot[ix] for dot in img] for img in dots]
 #ys = [ [ dot[iy] for dot in img] for img in dots]
@@ -38,33 +92,31 @@ isid = 5    # static id (tracks particles)
 #pl.axes().set_aspect(1)
 #pl.show()
 
-nslice = int(data[len(data)-1][islice])
+nparticles = int(max(data[:,isid]))
+for part in range(nparticles):
+    thispart = np.nonzero(data[:,isid]==part+1)
+    c = cm.spectral(1-float(part)/nparticles)
+    pl.figure(1)
+    pl.plot(data[thispart,ix],600-data[thispart,iy],'o',color=c,label=str(part))
 
-for slice in range(nslice):
-    prev = np.nonzero(data[:,islice]==slice)
-    curr = np.nonzero(data[:,islice]==slice+1)
+    # Find Squared Displacement
+    pl.figure(2)
+    thispart = np.nonzero(data[:,isid]==part+1)
+    #initx = data[thispart[0][0],ix]
+    #inity = data[thispart[0][0],iy]
+    thispartxs = data[thispart,ix]
+    thispartys = data[thispart,iy]
+    pl.loglog(data[np.nonzero(data[:,isid]==part+1)][:,idisp],color=c,label=str(part))
 
-    # for first image, use original particle ID as statid ID
-    if slice == 0:
-        data[curr,isid] = data[curr,iid]
-    else:
-        for newdot in data[curr]:
-            dist=1000.
-            for olddot in data[prev]:
-                newdist = (newdot[ix]-olddot[ix])**2 + (newdot[iy]-olddot[iy])**2
-                if newdist < dist:
-                    dist = newdist 
-                    #print "Closer"
-                    #print "new",newdot
-                    #print "old",olddot
-                    #newdot[isid] = olddot[isid] ### why doesn't this update data????
-                    data[newdot[iid]-1,isid] = olddot[isid] #must do this; previous line doesn't work
-                    #print "Now:", newdot
-                    #print "data:"
-                    #print data[curr]
-
-
-
+# Mean Squared Displacement:
+pl.loglog(msqdisp,'ko')
+pl.loglog(np.arange(nslice)+1,np.arange(nslice)+1,'k--')
+pl.legend()
+pl.xlabel('Time\n(Image frames)')
+pl.ylabel('Squared Displacement\n'+r'$pixels^2$')
+pl.figure(1)
+pl.imshow(bgimage,origin='lower')
+pl.show()
 
 
 
