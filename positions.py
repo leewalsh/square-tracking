b2abec3d04f4c84318de6170fae9791eb599bbe6
Lee Walsh <lawalsh@physics.umass.edu>
2013-06-25 01:40:06 -0400

    IT WORKS!!!!!!!!!
    	modified:   positions.py

--- positions.py
+++ positions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
-from scipy.ndimage import gaussian_filter, binary_erosion, convolve
+from scipy.ndimage import gaussian_filter, median_filter, binary_erosion, convolve, center_of_mass
 from skimage import filter, measure, segmentation
 from skimage.morphology import label, square, binary_closing, skeletonize
 from skimage.morphology import disk as _disk
@@ -10,7 +10,6 @@ import PIL.Image as image
 
 def label_particles_edge(im, sigma=2, closing_size=0, **extra_args):
     """ label_particles_edge(image, sigma=3, closing_size=3)
-
         Returns the labels for an image.
         Segments using Canny edge-finding filter.
 
@@ -24,13 +23,12 @@ def label_particles_edge(im, sigma=2, closing_size=0, **extra_args):
         edges = binary_closing(edges, square(closing_size))
     edges = skeletonize(edges)
     labels = label(edges)
-    print "found {} segments".format(max(labels.flatten()))
+    print "found {} segments".format(labels.max())
     labels = np.ma.array(labels, mask=edges==0) # in ma.array mask, False is True, and vice versa
     return labels
 
 def label_particles_walker(im, min_thresh=0.3, max_thresh=0.5, sigma=3):
     """ label_particles_walker(image, min_thresh=0.3, max_thresh=0.5)
-
         Returns the labels for an image.
         Segments using random_walker method.
 
@@ -39,40 +37,47 @@ def label_particles_walker(im, min_thresh=0.3, max_thresh=0.5, sigma=3):
         min_thresh   -- The lower limit for binary threshold
         max_thresh   -- The upper limit for binary threshold
     """
-    im = gaussian_filter(im, sigma)
+    if sigma>0:
+        im = gaussian_filter(im, sigma)
     labels = np.zeros_like(im)
-    labels[im>max_thresh*im.max()] = 1
-    labels[im<min_thresh*im.max()] = 2
-    return label(segmentation.random_walker(im, labels))
+    labels[im<min_thresh*im.max()] = 1
+    labels[im>max_thresh*im.max()] = 2
+    return segmentation.random_walker(im, labels)
 
-def label_particles_thresh(im, max_thresh=0.2, min_thresh=None):
-    """ label_particles_thresh(im, max_thresh=0.2, min_thresh=0)
-
+def label_particles_convolve(im, thresh=2, rmv=None, **extra_args):
+    """ label_particles_convolve(im, thresh=2)
         Returns the labels for an image
-        Segments using a simple binary threshold
+        Segments using a threshold after convolution with proper gaussian kernel
         Uses center of mass from original image to find centroid of segments
 
         Input:
-            image
-            max_thresh  the threshold above which pixels are included
-            min_thresh  if supplied, pixels are included if between min_thresh and max_thresh
+            image   the original image
+            pos     if given, the positions at which to remove large dots
+            thresh  the threshold above which pixels are included
     """
-    if min_thresh is not None:
-        threshed = (min_thresh < im) & (im < max_thresh)
-    else:
-        threshed = im > max_thresh
-    return label(threshed)
+    if rmv is not None:
+        im = remove_disks(im, rmv, disk(8.5))
+    convolved = convolve(im, gdisk(2))
+    convolved -= convolved.min()
+    convolved /= convolved.max()
 
-Particle = namedtuple('Particle', 'x y label ecc area'.split())
+    if isinstance(thresh, int):
+        thresh = convolved.mean() + thresh*convolved.std()
 
-def filter_particles(labels, max_ecc=0.5, min_area=15, max_area=200, **extra_args):
-    """ filter_particles(labels, max_ecc=0.5, min_area=15, max_area=200) -> [Particle]
+    labels = label(convolved > thresh)
+    print "found {} segments above thresh".format(labels.max())
+    return labels
 
+Segment = namedtuple('Segment', 'x y label ecc area'.split())
+
+def filter_segments(labels, max_ecc=0.5, min_area=15, max_area=200, intensity=None, **extra_args):
+    """ filter_segments(labels, max_ecc=0.5, min_area=15, max_area=200) -> [Segment]
         Returns a list of Particles and masks out labels for
         particles not meeting acceptance criteria.
     """
     pts = []
-    rprops = measure.regionprops(labels, ['Area', 'Eccentricity', 'Centroid'])
+    centroid = 'Centroid' if intensity is None else 'WeightedCentroid'
+    rprops = measure.regionprops(labels, ['Area', 'Eccentricity', centroid], intensity)
     for props in rprops:
         label = props['Label']
         if min_area > props['Area'] or props['Area'] > max_area \
@@ -80,35 +85,32 @@ def filter_particles(labels, max_ecc=0.5, min_area=15, max_area=200, **extra_arg
             pass
             #labels[labels==label] = np.ma.masked
         else:
-            (x,y) = props['Centroid']
-            pts.append(Particle(x,y, label, props['Eccentricity'], props['Area']))
+            x, y = props[centroid]
+            pts.append(Segment(x, y, label, props['Eccentricity'], props['Area']))
     return pts
 
-def drop_labels(labels, take_labels):
-    a = np.zeros_like(labels, dtype=bool)
-    for l in take_labels:
-        a = np.logical_or(a, labels == l)
-    labels[np.logical_not(a)] = np.ma.masked
-    return labels
-
 def find_particles(im, method='edge', **kwargs):
-    """ find_particles(im, gaussian_size=3, **kwargs) -> [Particle],labels
-
+    """ find_particles(im, gaussian_size=3, **kwargs) -> [Segment],labels
         Find the particles in image im. The arguments in kwargs is
-        passed to label_particles and filter_particles.
+        passed to label_particles and filter_segments.
 
         Returns the list of found particles and the label image.
     """
     labels = None
+    intensity = None
     print "Seeking particles using", method
     if method == 'walker':
         labels = label_particles_walker(im, **kwargs)
     elif method == 'edge':
         labels = label_particles_edge(im, **kwargs)
+    elif method == 'convolve':
+        labels = label_particles_convolve(im, **kwargs)
+        intensity = im
     else:
         raise RuntimeError('Undefined method "%s"' % method)
-    particles = filter_particles(labels, **kwargs)
-    return (particles,labels)
+
+    segments = filter_segments(labels, intensity=intensity, **kwargs)
+    return (segments, labels)
 
 def find_particles_in_image(f, **kwargs):
     """ find_particles_in_image(im, **kwargs)
@@ -116,8 +118,26 @@ def find_particles_in_image(f, **kwargs):
     print "opening", f
     im = image.open(f)
     im = np.array(im, dtype=float)
+    im = median_filter(im, size=2)
     im = im / im.max()
     return find_particles(im, **kwargs)
+
+def disk(n):
+    return _disk(n).astype(int)
+
+def gdisk(n, w=None):
+    """ gdisk(n):
+        return a gaussian kernel
+    """
+    if w is None:
+        w = 2*n
+    g = np.arange(-w, w+1)
+    g = np.exp(-.5 * g**2 / n**2)
+    g = np.outer(g, g)  # or g*g[...,None]
+    g -= g.mean()
+    g /= g.std()
+    assert np.allclose(g.sum(),0), 'sum is nonzero: {}'.format(g.sum())
+    return g
 
 def remove_segments(orig, particles, labels):
     """ remove_segments(orig, particles, labels)
@@ -125,43 +145,25 @@ def remove_segments(orig, particles, labels):
     """
     return
 
-def disk(n):
-    return _disk(n).astype(int)
-
-def gdisk(n):
-    w = 2*n
-    g = np.arange(-w, w+1)
-    g = np.exp(-.5 * g**2 / n**2)
-    g = np.outer(g, g)  # or g*g[...,None]
-    g -= g.mean()
-    g /= g.std()
-    assert g.sum() == 0, 'sum is nonzero: {}'.format(g.sum())
-    return g
-
 def remove_disks(orig, particles, dsk=disk(6)):
     """ remove_disks(method=['disk' or 'segment'])
         removes a disk of given size centered at dot location
         inputs:
             orig   -    input image as ndarray or PIL Image
-            particles - list of particles (namedtuple Particle)
+            particles - list of particles (namedtuple Segment)
             dsk   -    radius of disk, default is skimage.morphology.disk(r)
                             (size of square array is 2*r+1)
         output:
             the original image with big dots removed
     """
     disks = np.ones(orig.shape, int)
-    if isinstance(particles[0], Particle):
-        xys = zip(*(map(int,(p.x,p.y)) for p in pts))
+    if isinstance(particles[0], Segment):
+        xys = zip(*(map(int,(p.x,p.y)) for p in particles))
     elif 'X' in particles.dtype.names:
         xys = np.round(particles['X']).astype(int),np.round(particles['Y']).astype(int)
     disks[xys] = 0
     disks = binary_erosion(disks,dsk)
     return orig*disks
-
-def convolve_disks(orig, dsk):
-    if isinstance(dsk, int):
-        dsk = gdisk(dsk)
-    return convolve(orig, dsk)
 
 if __name__ == '__main__':
     import matplotlib
@@ -192,25 +194,38 @@ if __name__ == '__main__':
     threshargs = {'max_ecc' :  .4 if args.slr else  .7,
                   'min_area': 160 if args.slr else  15,
                   'max_area': 250 if args.slr else 200}
+    cthreshargs = {'max_ecc' :  .4 if args.slr else .7,
+                   'min_area': 160 if args.slr else  4,
+                   'max_area': 250 if args.slr else 24}
 
     def f((n,filename)):
-        pts, labels = find_particles_in_image(filename, **threshargs)
-        print '%20s: Found %d points' % ('', len(pts))
-        if args.corner:
-            diff = remove_disks()
-            #TODO pass removed image to find corners
-            if args.plot:
-                #TODO plot stuff with corner
-                pass 
-        elif args.plot:
+        pts, labels = find_particles_in_image(filename, method='edge', **threshargs)
+        centers = np.hstack([n*np.ones((len(pts),1)), pts])
+        print '%20s: Found %d particles' % ('', len(pts))
+        if args.plot:
             pl.clf()
             pl.imshow(labels, cmap=cm)
-            pts = np.asarray(pts)
-            pl.scatter(pts[:,1], pts[:,0], c=pts[:,2], cmap=cm)
+            ptsarr = np.asarray(pts)
+            pl.scatter(ptsarr[:,1], ptsarr[:,0], c='r')#ptsarr[:,2], cmap=cm)
             savename = path.join(pdir,path.split(filename)[-1].split('.')[-2])+'_POSITIONS.png'
-            print 'saving to',savename
+            #print 'saving to',savename
             pl.savefig(savename, dpi=300)
-        return np.hstack([n*np.ones((len(pts),1)), pts])
+
+        if args.corner:
+            cpts, clabels = find_particles_in_image(filename,
+                                    method='convolve', rmv=pts, **cthreshargs)
+            print '%20s: Found %d corners' % ('', len(cpts))
+            if args.plot:
+                #pl.imshow(clabels, cmap=cm)
+                cpts = np.asarray(cpts)
+                pl.scatter(cpts[:,1], cpts[:,0], c='w')#cpts[:,2], cmap=cm)
+                #print 'saving corners to',savename
+                pl.savefig(savename, dpi=300)
+
+            corners = np.hstack([n*np.ones((len(cpts),1)), cpts])
+            return centers, corners
+
+        return centers
 
     filenames = sorted(args.files)
     if args.threads > 1:
@@ -219,10 +234,24 @@ if __name__ == '__main__':
         points = p.map(f, enumerate(filenames))
     else:
         points = map(f, enumerate(filenames))
-    points = np.vstack(points)
-    if args.corner and args.output == 'POSITIONS':
-        # change default for CORNER dot output
-        args.output = 'CORNER_POSITIONS'
+
+    if args.corner:
+        points, corners = map(np.vstack, zip(*points))
+        if 'POSITIONS' in args.output:
+            coutput = args.output.replace('POSITIONS','CORNER_POSITIONS')
+        else:
+            coutput = ''.join(args.output.split('.')[:-1]+['_CORNER.']+args.output.split('.')[-1:])
+        with open(coutput, 'w') as output:
+            output.write('# Frame    X           Y             Label  Eccen        Area\n')
+            np.savetxt(output, points, delimiter='     ',
+                    fmt=['%6d', '%7.3f', '%7.3f', '%4d', '%1.3f', '%5d'])
+    else:
+        points = np.vstack(points)
+
+    if 'CORNER_' in args.output:
+        args.output = args.output.replace('CORNER_','')
+    elif 'CORNER' in args.output:
+        args.output = args.output.replace('CORNER','')
     with open(args.output, 'w') as output:
         output.write('# Frame    X           Y             Label  Eccen        Area\n')
         np.savetxt(output, points, delimiter='     ',
