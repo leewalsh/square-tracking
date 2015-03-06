09a5bd3f61684711eeebb6d6dc7613e7308306ce
Michael Mueller <michaeldmueller7@gmail.com>
2015-03-06 18:53:33 -0500

    Changed analysis and simulation programs

--- analysis.py
+++ analysis.py
@@ -1,41 +1,176 @@
-import numpy as np
+#!/usr/bin/env python
 import sys
+import numpy as np
+from scipy.spatial import KDTree, Voronoi
+from math import sqrt
+
 import math
-import os
-from matplotlib import pyplot as plt
 
-def analyze(input_file, output_file, label):
-    particles = np.genfromtxt(input_file, dtype='i,f,f,i,f,i', names=True,
-                          skip_header=3)
-    variances = {}
+def get_angle(p, q):
+    # angle of (q - p) from the horizontal
+    r = (q[0] - p[0], q[1] - p[1])
+    return math.atan2(r[1], r[0])
 
-    if os.path.isfile(output_file):
-        variances = np.load(output_file).item()
-        plt.plot(variances.keys(), variances.values(), label=label)
-        return
-    num_particles = len([row for row in particles if row[0] == 0])
-    for frame in range(particles[-1][0] + 1):
-        print('Frame {0}'.format(frame))
-        frame_particles = [row for row in particles if row[0] == frame]
-        if len(frame_particles) != num_particles:
+def exp(C):
+    # C complex
+    return math.exp(C.real) * (math.cos(C.imag) + 1j * math.sin(C.imag))
+
+def poly_area(corners):
+    # calculate area of polygon
+    area = 0.0
+    n = len(corners)
+    for i in range(n):
+        j = (i + 1) % n
+        area += corners[i][0] * corners[j][1]
+        area -= corners[j][0] * corners[i][1]
+    return abs(area) / 2.0
+
+def find_COM(frame):
+    return (sum([p[0] for p in frame]) / len(frame),
+            sum([p[1] for p in frame]) / len(frame))
+
+M = 4 # number of neighbors
+
+if len(sys.argv) < 2:
+    print("Please specify a filename.")
+    sys.exit(0)
+
+fname = sys.argv[1]
+data = np.genfromtxt(fname + "_POSITIONS.txt", dtype="i,f,f,i,f,i",
+                     names="f,x,y,lab,ecc,area")
+frames = [[(row[1], row[2]) for row in data[data['f']==i]] for i in
+          range(data[-1][0] + 1)]
+frame_IDs = [[row[3] for row in data[data['f']==i]] for i in
+             range(data[-1][0] + 1)]
+psi_data = []
+frame_densities = []
+radial_psi = []
+radial_densities = []
+radial_r = []
+radial_speed = []
+valencies = {}
+initial_pos = {}
+
+# Calculate valency for each ID based on first frame
+COM = find_COM(frames[0])
+dists = [((row[1]-COM[0])**2 + (row[2]-COM[1])**2, row[3])
+         for row in data[data['f']==0]]
+sorted_dists = sorted(dists)
+n = 0
+i = 0
+while True:
+    n += 1
+    box_size = 8 * (n - 1) if n > 1 else 1
+    for j in range(box_size):
+        valencies[sorted_dists[i][1]] = n
+        i += 1
+        if i >= len(frames[0]):
+            break
+    else:
+        continue
+    break
+
+max_valency = max(valencies.values())
+# Find initial positions for each ID
+frame0 = data[data['f']==0]
+for x in frame0:
+    initial_pos[x[3]] = (x[1], x[2])
+
+for j, frame in enumerate(frames):
+    vor = Voronoi(frame)
+    areas = []
+    r_densities = []
+    r_psi = []
+    r_r = []
+
+    for v in range(max_valency + 1): # each list contains all s=k
+        r_densities.append([])
+        r_psi.append([])
+        r_r.append([])
+
+    for i, p in enumerate(vor.points):
+        region = vor.regions[vor.point_region[i]]
+        if -1 in region: # infinite Voronoi cell
             continue
-        positions = [(row[1], row[2]) for row in frame_particles]
-        center = (sum(p[0] for p in positions) / num_particles,
-                  sum(p[1] for p in positions) / num_particles)
-        distances = [math.sqrt((p[0] - center[0])**2 + (p[1] - center[1])**2)
-                     for p in positions]
-        avg_distance = sum(distances) / num_particles
-        variance = sum([(r - avg_distance)**2 for r in distances])
-        variances[frame] = variance
-    np.save(output_file, variances)
-    plt.plot(variances.keys(), variances.values(), label=label)
+        areas.append(poly_area([vor.vertices[q] for q in region]))
+        if areas[-1] > 0.:
+            r_densities[valencies[frame_IDs[j][i]]].append(1. / areas[-1])
+        
+    areas = np.asarray(areas)
+    densities = 1. / areas[areas > 0.]
+    frame_densities.append(densities)
+    radial_densities.append(r_densities)
 
-if len(sys.argv) > 1:
-    input_files = [("{0}.txt".format(x), "ANALYSIS_{0}.npy".format(x),
-                    x) for x in sys.argv[1:]]
-else:
-    input_files = [("POSITIONS", "ANALYSIS.npy", "Data")]
-for f, out_f, label in input_files:
-    analyze(f, out_f, label)
-plt.legend(loc='upper left')
-plt.show()
+    tree = KDTree(frame)
+    COM = find_COM(frame)
+    psi_frame = []
+    for i, p in enumerate(frame):
+        query_ret = tree.query([p], k=M+1)
+
+        # remove p from the neighbors list by slicing
+        neighbors = [tree.data[x] for x in query_ret[1][0]][1:]
+        # if p is an edge or corner, remove extra neighbors
+        min_dist = min((n[0]-p[0])**2 + (n[1]-p[1])**2 for n in neighbors)
+        thresh = min_dist * 2 * .9 # slightly less than a diagonal
+        neighbors = [n for n in neighbors if (n[0]-p[0])**2 + (n[1]-p[1])**2 < thresh]
+        N = len(neighbors)
+        psi = sum(exp(M * get_angle(p, n) * 1j) for n in neighbors) / N
+        valency = valencies[frame_IDs[j][i]]
+        if N > 1: # if N=1, |psi| will trivially be 1
+            psi_frame.append(abs(psi))
+            r_psi[valency].append(abs(psi))
+        r = sqrt((COM[0]-p[0])**2 + (COM[1]-p[1])**2)
+        r_r[valency].append(r)
+        p_0 = initial_pos[frame_IDs[j][i]]
+        squared_disp = (p[0]-p_0[0])**2 + (p[1]-p_0[1])**2
+
+    psi_data.append(psi_frame)
+    radial_psi.append(r_psi)
+    radial_r.append(r_r)
+
+# Calculate radial speed (not MSD!)
+tau = 1
+
+for t, frame in enumerate(frames[:-tau]):
+    disps = []
+    for v in range(max_valency + 1): # contains all s=k
+        disps.append([])
+    other_frame = frames[t + tau]
+    # range over all particles in frames t, t+tau
+    for i, p in enumerate(frames[t]):
+        for j, q in enumerate(other_frame):
+            if frame_IDs[t + tau][j] == frame_IDs[t][i]:
+                break
+        else:            
+            print("ID {0} not found in frame {1}!".format(
+                frame_IDs[t][i],t+tau))
+            continue
+        # found corresponding particle q in other frame
+        valency = valencies[frame_IDs[t][i]]
+        disps[valency].append(math.sqrt((p[0]-q[0])**2 + (p[1]-q[1])**2))
+    radial_speed.append(disps)
+
+max_density = max([max(densities) for densities in frame_densities])
+frame_densities = [densities / max_density for densities in frame_densities]
+frame_densities=[]
+
+#take averages
+def take_avg(stat, ignore_first):
+    if ignore_first:
+        for frame in stat:
+            frame[2].extend(frame[1])
+            frame[1] = []
+    ret = [[sum(l)/len(l) if len(l)>0 else -1.
+            for l in frame[1:]] for frame in stat]
+    return [x[1:] for x in ret] if ignore_first else ret
+
+radial_psi = take_avg(radial_psi, True)
+radial_densities = take_avg(radial_densities, True)
+radial_r = take_avg(radial_r, True)
+radial_speed = take_avg(radial_speed, True)
+
+m = max([max(x) for x in radial_densities])
+radial_densities /= m
+np.savez(fname + "_DATA.npz", psi=psi_data, densities=frame_densities,
+         radial_psi=radial_psi, radial_densities=radial_densities,
+         radial_r=radial_r, radial_speed=radial_speed)
--- simulation.py
+++ simulation.py
@@ -10,6 +10,11 @@ if len(sys.argv) < 2:
     print("Please specify a filename")
     sys.exit(0)
 
+DX = 300
+DY = 300
+MSD_TEST = False
+L = 5
+
 pygame.init()
 screen = pygame.display.set_mode((608, 600))
 pygame.display.set_caption("Particle movement simulation")
@@ -31,6 +36,31 @@ for row in positions:
     frame.append((row[1], row[2], row[3]))
 frames.append(frame)
 
+def find_COM(frame):
+    return (sum([p[0] for p in frame]) / len(frame),
+            sum([p[1] for p in frame]) / len(frame))
+
+COM = find_COM(frames[0])
+dists = [((row[1]-COM[0])**2 + (row[2]-COM[1])**2, row[3])
+         for row in positions if row[0]==0]
+sorted_dists = sorted(dists)
+n = 0
+i = 0
+valencies = {}
+while True:
+    n += 1
+    box_size = 8 * (n - 1) if n > 1 else 1
+    for j in range(box_size):
+        valencies[sorted_dists[i][1]] = n
+        i += 1
+        if i >= len(frames[0]):
+            break
+    else:
+        continue
+    break
+
+max_valency = max(valencies.values())
+
 running = True
 simulate = False
 t = 0
@@ -39,8 +69,14 @@ paused = False
 if len(sys.argv) > 2:
     speed = float(sys.argv[2])
 font = pygame.font.Font(None, 36)
-COLORS = [(random() * 255, random() * 255, random() * 255)
-          for i in range(len(frames[0]))]
+font2 = pygame.font.Font(None, 15)
+COLORS = {}
+COLORS2 = {}
+for ID in valencies:
+    s = valencies[ID]
+    COLORS[ID] = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)][s-1]
+    COLORS2[ID] = [(100, 0, 0), (0, 100, 0), (0, 0, 100), (0, 100, 100)][s-1]
+tau = 5
 
 while running and t < len(frames):
     draw = simulate and not paused
@@ -66,7 +102,22 @@ while running and t < len(frames):
     screen.blit(text, text.get_rect())
     
     for x, y, ID in frames[t]:
-        pygame.draw.rect(screen, COLORS[ID], (x-5, y-5, 10, 10), 0)
+        pygame.draw.rect(screen, COLORS[ID], (2*x-L-DX, 2*y-L-DY,2*L,2*L), 0)
+
+    if MSD_TEST and t+tau<len(frames):
+        for x, y, ID in frames[t+tau]:
+            for x0, y0, ID0 in frames[t]:
+                if ID==ID0:
+                    break
+            else:
+                print("ID {0} has a disconnect from frames {1} to {2}!".
+                      format(ID, t, t+tau))
+                continue
+            pygame.draw.rect(screen, COLORS2[ID], (2*x-L-DX,2*y-L-DY,2*L,2*L), 0)
+            pygame.draw.line(screen, (0,0,0), (2*x-DX,2*y-DY),(2*x0-DX,2*y0-DY))
+            dist = (x0-x)**2 + (y0-y)**2
+            lbl = font2.render("%.1f"%(dist), 1, (0, 0, 0))
+            screen.blit(lbl, (2*x-DX,2*y-DY+10))
     pygame.display.flip()
     if not paused:
         t += 1
