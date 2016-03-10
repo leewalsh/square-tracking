522d1e4f9df618ae50995dca58021ace3e1da69a
Lee Walsh <lawalsh@physics.umass.edu>
2016-03-10 17:26:20 -0500

    Update velocity histogram plot, save values to META

--- velocity.py
+++ velocity.py
@@ -38,14 +38,16 @@ if __name__ == '__main__':
     arg('--particle', type=str, default='', help='Particle name')
     arg('--save', type=str, nargs='?', const='velocity', default='',
         help='Save figure (optionally provide suffix)?')
-    arg('--lin', action='store_false', dest='log', help='Plot on linear scale?')
+    arg('--lin', action='store_true', help='Plot on linear scale?')
     arg('--log', action='store_true', help='Plot on a log scale?')
     arg('--dupes', action='store_true', help='Remove duplicates from tracks')
     arg('--normalize', action='store_true', help='Normalize by max?')
     arg('--autocorr', action='store_true', help='Plot <vv> autocorrelation?')
     arg('--untrackorient', action='store_false', dest='torient',
         help='Untracked raw orientation (mod 2pi)?')
-    arg('--interp', action='store_true', dest='interp', help='Interpolate gaps')
+    arg('-g', '--gaps', choices=['interp', 'nans', 'leave'], default='interp',
+        nargs='?', const='nans', help="Gap handling: choose from %(choices)s. "
+        "default is %(default)s, `-g` or `--gaps` alone gives %(const)s")
     arg('--stub', type=int, default=10, help='Min track length. Default: 10')
     arg('--nosubtract', action='store_false', dest='subtract',
         help="Don't subtract v0?")
@@ -88,8 +90,7 @@ def noise_derivatives(tdata, width=(1,), side=1, fps=1, xy=False,
 
 def compile_noise(prefixes, vs, width=(1,), side=1, fps=1, cat=True,
                   do_orientation=True, do_translation=True, subtract=True,
-                  stub=10, torient=True, interp=True, dupes=False, **ignored):
-    print sorted(ignored)
+                  stub=10, torient=True, gaps='interp', dupes=False, **ignored):
     if np.isscalar(prefixes):
         prefixes = [prefixes]
     for prefix in prefixes:
@@ -100,7 +101,7 @@ def compile_noise(prefixes, vs, width=(1,), side=1, fps=1, cat=True,
             data['t'] = tracks.remove_duplicates(data['t'], data)
         tracksets = helpy.load_tracksets(data, min_length=stub,
                                          run_track_orient=torient,
-                                         run_fill_gaps=interp)
+                                         run_fill_gaps=gaps)
         for track in tracksets:
             tdata = tracksets[track]
             velocities = noise_derivatives(
@@ -122,7 +123,7 @@ def get_stats(a):
     M = np.nanmean(a, -1, keepdims=a.ndim > 1)
     # c = a - M
     # variance = np.einsum('...j,...j->...', c, c)/n
-    variance = np.nanvar(a, -1, keepdims=a.ndim > 1)
+    variance = np.nanvar(a, -1, keepdims=a.ndim > 1, ddof=1)
     D = 0.5*variance
     SE = np.sqrt(variance)/sqrt(n - 1)
     return M, D, SE
@@ -165,33 +166,57 @@ def plot_widths(widths, stats, normalize=False):
     return fig
 
 
-def plot_hist(a, nax=1, axi=1, bins=100, log=True, orient=False, label='v',
-              title='', subtitle=''):
+def plot_hist(a, nax=(1, 1), axi=1, bins=100, log=True, lin=True, orient=False,
+              label='v', title='', subtitle=''):
     stats = get_stats(a)
-    ax = axi[0] if isinstance(axi, tuple) else plt.subplot(nax, 2, axi*2-1)
-    bins = ax.hist(a, bins, log=False, alpha=0.7,
-            label=('$\\langle {} \\rangle = {:.5f}$\n'
-                   '$D = {:.5f}$\n'
-                   '$\\sigma/\\sqrt{{N}} = {:.5f}$').format(label, *stats))[1]
-    ax.legend(loc='upper left', fontsize='xx-small', frameon=False)
+    nrows, ncols = nax
+    if isinstance(axi, tuple):
+        ax = axi[0]
+    else:
+        ax = plt.subplot(nrows, ncols, (axi - 1)*ncols + 1)
+    label = '\n'.join(['$\\langle {val} \\rangle = {0:.5f}$',
+                       '$\ D_{sub}\  = {1:.5f}$',
+                       '$\\sigma/\\sqrt{{N}} = {2:.5f}$'][:2]).format(
+                           *stats[:2], **label)
+    counts, bins, _ = ax.hist(a, bins, log=False, alpha=0.7, label=label)
+    plot_gaussian(stats[0], stats[1], bins, counts.sum(), ax)
+    ax.legend(loc='upper left', fontsize='small', frameon=False)
     ax.set_ylabel('Frequency')
+    l, r = ax.set_xlim(bins[0], bins[-1])
     if orient:
-        l, r = ax.set_xlim(bins[0], bins[-1])
         xticks = np.linspace(l, r, 5)
         ax.set_xticks(xticks)
-        ax.set_xticklabels(map('${:.2f}\pi$'.format, xticks/pi), fontsize='small')
+        xticklabels = map('${:.2f}\pi$'.format, xticks/pi)
+        ax.set_xticklabels(xticklabels, fontsize='small')
     xlabel = 'Velocity ({}/vibration)'.format('rad' if orient else 'particle')
     ax.set_xlabel(xlabel)
-    ax.set_title("{} ({})".format(title, subtitle), fontsize='medium')
-
-    ax2 = axi[1] if isinstance(axi, tuple) else plt.subplot(nax, 2, axi*2)
-    bins = ax2.hist(a, bins*2, log=True, alpha=0.7)[1]
+    ax.set_title(" ".join([title, subtitle]), fontsize='medium')
+    if ncols < 2:
+        return stats, (ax,)
+    if isinstance(axi, tuple):
+        ax2 = axi[1]
+    else:
+        ax2 = plt.subplot(nrows, ncols, axi*ncols)
+    counts, bins, _ = ax2.hist(a, bins*2, log=True, alpha=0.7)
+    plot_gaussian(stats[0], stats[1], bins, counts.sum(), ax2)
     if orient:
         l, r = ax2.set_xlim(bins[0], bins[-1])
         xticks = np.linspace(l, r, 9)
         ax2.set_xticks(xticks)
-        ax2.set_xticklabels(map('${:.2f}\pi$'.format, xticks/pi), fontsize='small')
-    return ax, ax2
+        xticklabels = map('${:.2f}\pi$'.format, xticks/pi)
+        ax2.set_xticklabels(xticklabels, fontsize='small')
+    ax2.set_ylim(1, 10**int(1 + np.log10(counts.max())))
+    return stats, (ax, ax2)
+
+
+def plot_gaussian(M, D, bins, count=1, ax=None):
+    ax = ax or plt.gca()
+    dx = bins[1] - bins[0]
+    var = 2*D
+    g = np.exp(-0.5 * (bins-M)**2 / var)
+    g /= sqrt(2*pi*var) / (dx*count)
+    ax.plot(bins, g, c=(0.25, 0.5, 0), lw=3)
+
 
 def vv_autocorr(prefixes, corrlen=0.5, **compile_args):
     vs = defaultdict(list)
@@ -226,8 +251,8 @@ if __name__ == '__main__':
     helpy.save_log_entry(args.prefix, 'argv')
     meta = helpy.load_meta(args.prefix)
     if args.verbose:
-        print 'using'
-        print '\n'.join([prefixes] if np.isscalar(prefixes) else prefixes)
+        print 'prefixes:',
+        print '\n          '.join(np.atleast_1d(prefixes))
     helpy.sync_args_meta(args, meta, ['side', 'fps'],
                          ['sidelength', 'fps'], [1, 1])
     compile_args = dict(args.__dict__)
@@ -249,33 +274,48 @@ if __name__ == '__main__':
         ax.set_title(r"Velocity Autocorrelation $\langle v(t) v(0) \rangle$")
         ax.legend(loc='best')
     else:
+        meta.update(vel_stub=args.stub, vel_gaps=args.gaps,
+                    vel_dx_width=args.width[0])
         vs = defaultdict(list)
         trackcount = compile_noise(prefixes, vs, **compile_args)
 
-        nax = args.do_orientation + args.do_translation*(args.subtract + 1)
+        nax = (args.do_orientation + args.do_translation*(args.subtract + 1),
+               args.log + args.lin)
         axi = 1
         subtitle = args.particle
         bins = np.linspace(-1, 1, 51)
+        brange = 0.5
         if args.do_orientation:
-            plot_hist(vs['o'], nax, axi, bins=bins*pi/2, log=args.log,
-                      label=r'\xi', orient=True, title='Orientation',
-                      subtitle=subtitle)
+            title = 'Orientation'
+            label = {'val': r'\xi', 'sub': 'R'}
+            stats, axes = plot_hist(vs['o'], nax, axi, bins=bins*pi/2,
+                                    lin=args.lin, log=args.log, label=label,
+                                    orient=True, title=title, subtitle=subtitle)
+            # could also save fit_vo_w0=stats[0])
+            meta.update(fit_vo_DR=stats[1])
             axi += 1
         if args.do_translation:
-            ax, ax2 = plot_hist(vs['par'], nax, axi, log=args.log,
-                                label='v_\parallel', bins=bins)
-            plot_hist(vs['perp'], nax, (ax, ax2), log=args.log, label='v_\perp',
-                      bins=bins, title='Parallel & Transverse',
-                      subtitle=subtitle)
+            title = 'Parallel & Transverse'
+            label = {'val': 'v_\perp', 'sub': '\perp'}
+            stats, axes = plot_hist(vs['perp'], nax, axi, bins=bins*brange,
+                                    lin=args.lin, log=args.log, label=label,
+                                    title=title, subtitle=subtitle)
+            meta.update(fit_vt_DT=stats[1])
+            label = {'val': 'v_\parallel', 'sub': '\parallel'}
+            stats, axes = plot_hist(vs['par'], nax, axes, bins=bins*brange,
+                                    lin=args.lin, log=args.log, label=label)
+            meta.update(fit_vn_v0=stats[0], fit_vn_DT=stats[1])
             axi += 1
             if args.subtract:
                 plot_hist(np.concatenate([vs['etapar'], vs['perp']]), nax, axi,
-                          log=args.log, label=r'\eta_\alpha', bins=bins,
-                          title='$v_0$ subtracted', subtitle=subtitle)
+                          lin=args.lin, log=args.log, label=r'\eta_\alpha',
+                          bins=bins, title='$v_0$ subtracted',
+                          subtitle=subtitle)
                 axi += 1
 
     if args.save:
         savename = os.path.abspath(args.prefix.rstrip('/._?*'))
+        helpy.save_meta(savename, meta)
         savename += '_' + args.save + '.pdf'
         print 'Saving plot to {}'.format(savename)
         plt.savefig(savename)
