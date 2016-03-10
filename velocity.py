929f9a996a6b6a473c8ada8486249e4399a39c28
Lee Walsh <lawalsh@physics.umass.edu>
2016-03-10 13:17:28 -0500

    argument options fixed in velocity
     - doesn't go looking for sarah's data in her directory structure
     - can give list of widths now to check derivative smoothing width
     - can give wildcards as prefix, or just a regular prefix and it doesn't
       search with a glob. this is better for consistency with using sets
       merged with helpy.merge_data as in the correlation functions.

--- velocity.py
+++ velocity.py
@@ -3,6 +3,18 @@
 
 from __future__ import division
 
+import os
+from collections import defaultdict
+from glob import iglob
+from math import sqrt
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import helpy
+import tracks
+import correlation as corr
+
 description = """This script plots a histogram of the velocity noise for one or
 several data sets. Includes option to subtract v_0 from translational noise.
 The histogram figure is optionally saved to file prefix.plothist[orient].pdf
@@ -10,7 +22,7 @@ Run from the folder containing the positions file.
 Copyright (c) 2015 Sarah Schlossberg, Lee Walsh; all rights reserved.
 """
 
-if __name__=='__main__':
+if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser(description=description)
     arg = parser.add_argument
@@ -19,10 +31,10 @@ if __name__=='__main__':
         dest='do_translation', help='Only orientational noise?')
     arg('-t', '--translation', action='store_false',
         dest='do_orientation', help='Only translational noise?')
-    arg('--sets', type=int, default=1, metavar='N', nargs='?', const=0,
+    arg('--sets', type=int, default=0, metavar='N', nargs='?', const=1,
         help='Number of sets')
-    arg('--width', type=float, default=0.75, metavar='W', nargs='?', const=-.5,
-        help='Smoothing width for derivative')
+    arg('--width', type=float, default=(0.75,), metavar='W', nargs='*',
+        help='Smoothing width for derivative, may give several')
     arg('--particle', type=str, default='', help='Particle name')
     arg('--save', type=str, nargs='?', const='velocity', default='',
         help='Save figure (optionally provide suffix)?')
@@ -37,34 +49,27 @@ if __name__=='__main__':
     arg('--stub', type=int, default=10, help='Min track length. Default: 10')
     arg('--nosubtract', action='store_false', dest='subtract',
         help="Don't subtract v0?")
-    arg('-s', '--side', type=float, default=1,
+    arg('-s', '--side', type=float,
         help='Particle size in pixels, for unit normalization')
-    arg('-f', '--fps', type=float, default=1, help="Number frames per shake "
+    arg('-f', '--fps', type=float, help="Number frames per shake "
         "(or second) for unit normalization.")
     arg('-v', '--verbose', action='count', help="Be verbose")
     args = parser.parse_args()
 
-import os
-from collections import defaultdict
-from math import sqrt
-import numpy as np
-import matplotlib.pyplot as plt
-import helpy, tracks, correlation as corr
-
 pi = np.pi
 
-def noise_derivatives(tdata, width=1, side=1, fps=1, xy=False,
+
+def noise_derivatives(tdata, width=(1,), side=1, fps=1, xy=False,
                       do_orientation=True, do_translation=True, subtract=True):
     x = tdata['f']/fps
     ret = {}
-    ws = [width] if np.isscalar(width) else width
     if do_orientation:
         ret['o'] = np.array([helpy.der(tdata['o'], x=x, iwidth=w)
-                             for w in ws]).squeeze()
+                             for w in width]).squeeze()
     if do_translation:
         cos, sin = np.cos(tdata['o']), np.sin(tdata['o'])
         vx, vy = [np.array([helpy.der(tdata[i]/side, x=x, iwidth=w)
-                            for w in ws]).squeeze() for i in 'xy']
+                            for w in width]).squeeze() for i in 'xy']
         if xy:
             ret['x'], ret['y'] = vx, vy
         else:
@@ -72,7 +77,7 @@ def noise_derivatives(tdata, width=1, side=1, fps=1, xy=False,
             vT = vx*sin - vy*cos
             ret['par'], ret['perp'] = vI, vT
         if subtract:
-            v0 = vI.mean(-1, keepdims=vI.ndim>1)
+            v0 = vI.mean(-1, keepdims=vI.ndim > 1)
             if xy:
                 ret['etax'] = vx - v0*cos
                 ret['etay'] = vy - v0*sin
@@ -80,9 +85,11 @@ def noise_derivatives(tdata, width=1, side=1, fps=1, xy=False,
                 ret['etapar'] = vI - v0
     return ret
 
-def compile_noise(prefixes, vs, width=3, side=1, fps=1, cat=True,
+
+def compile_noise(prefixes, vs, width=(1,), side=1, fps=1, cat=True,
                   do_orientation=True, do_translation=True, subtract=True,
                   stub=10, torient=True, interp=True, dupes=False, **ignored):
+    print sorted(ignored)
     if np.isscalar(prefixes):
         prefixes = [prefixes]
     for prefix in prefixes:
@@ -92,12 +99,13 @@ def compile_noise(prefixes, vs, width=3, side=1, fps=1, cat=True,
         if dupes:
             data['t'] = tracks.remove_duplicates(data['t'], data)
         tracksets = helpy.load_tracksets(data, min_length=stub,
-                run_track_orient=torient, run_fill_gaps=interp)
+                                         run_track_orient=torient,
+                                         run_fill_gaps=interp)
         for track in tracksets:
             tdata = tracksets[track]
-            velocities = noise_derivatives(tdata, width=width,
-                    side=side, fps=fps, do_orientation=do_orientation,
-                    do_translation=do_translation, subtract=subtract)
+            velocities = noise_derivatives(
+                tdata, width=width, side=side, fps=fps, subtract=subtract,
+                do_orientation=do_orientation, do_translation=do_translation)
             for v in velocities:
                 vs[v].append(velocities[v])
     if cat:
@@ -105,8 +113,10 @@ def compile_noise(prefixes, vs, width=3, side=1, fps=1, cat=True,
             vs[v] = np.concatenate(vs[v], -1)
     return len(tracksets)
 
+
 def get_stats(a):
-    #Computes mean, D_T or D_R, and standard error for a list.
+    """Computes mean, D_T or D_R, and standard error for a list.
+    """
     a = np.asarray(a)
     if a.ndim==1:
         a = a[np.isfinite(a)]
@@ -120,23 +130,24 @@ def get_stats(a):
     SE = np.sqrt(variance)/sqrt(n)
     return M, D, SE
 
-def compile_widths(widths, prefixes, **compile_args):
-    stats = {v: {s: np.empty_like(widths)
+
+def compile_widths(prefixes, **compile_args):
+    stats = {v: {s: np.empty_like(compile_args['width'])
                  for s in 'mean var stderr'.split()}
              for v in 'o par perp etapar'.split()}
-    compile_args['width'] = widths
     vs = defaultdict(list)
     compile_noise(prefixes, vs, **compile_args)
     for v, s in stats.items():
         s['mean'], s['var'], s['stderr'] = get_stats(vs[v])
     return stats
 
+
 def plot_widths(widths, stats, normalize=False):
     ls = {'o': '-', 'par': '-.', 'perp': ':', 'etapar': '--'}
     cs = {'mean': 'r', 'var': 'g', 'stderr': 'b'}
     label = {'o': r'$\xi$', 'par': r'$v_\parallel$', 'perp': r'$v_\perp$',
              'etapar': r'$\eta_\parallel$'}
-    fig = plt.figure(figsize=(8,12))
+    fig = plt.figure(figsize=(8, 12))
     for i, s in enumerate(stats['o']):
         ax = fig.add_subplot(len(stats['o']), 1, i+1)
         for v in stats:
@@ -156,7 +167,9 @@ def plot_widths(widths, stats, normalize=False):
         ax.legend(loc='best')
     return fig
 
-def plot_hist(a, nax=1, axi=1, bins=100, log=True, orient=False, label='v', title='', subtitle=''):
+
+def plot_hist(a, nax=1, axi=1, bins=100, log=True, orient=False, label='v',
+              title='', subtitle=''):
     stats = get_stats(a)
     ax = axi[0] if isinstance(axi, tuple) else plt.subplot(nax, 2, axi*2-1)
     bins = ax.hist(a, bins, log=False, alpha=0.7,
@@ -169,16 +182,18 @@ def plot_hist(a, nax=1, axi=1, bins=100, log=True, orient=False, label='v', titl
         l, r = ax.set_xlim(bins[0], bins[-1])
         xticks = np.linspace(l, r, 5)
         ax.set_xticks(xticks)
-        ax.set_xticklabels(['${:.2f}\pi$'.format(x) for x in xticks/pi], fontsize='small')
-    ax.set_xlabel('Velocity ({}/vibation)'.format('rad' if orient else 'particle'))
+        ax.set_xticklabels(map('${:.2f}\pi$'.format, xticks/pi), fontsize='small')
+    xlabel = 'Velocity ({}/vibration)'.format('rad' if orient else 'particle')
+    ax.set_xlabel(xlabel)
     ax.set_title("{} ({})".format(title, subtitle), fontsize='medium')
+
     ax2 = axi[1] if isinstance(axi, tuple) else plt.subplot(nax, 2, axi*2)
     bins = ax2.hist(a, bins*2, log=True, alpha=0.7)[1]
     if orient:
         l, r = ax2.set_xlim(bins[0], bins[-1])
         xticks = np.linspace(l, r, 9)
         ax2.set_xticks(xticks)
-        ax2.set_xticklabels(['${:.2f}\pi$'.format(x) for x in xticks/pi], fontsize='small')
+        ax2.set_xticklabels(map('${:.2f}\pi$'.format, xticks/pi), fontsize='small')
     return ax, ax2
 
 def vv_autocorr(prefixes, corrlen=0.5, **compile_args):
@@ -198,48 +213,44 @@ def vv_autocorr(prefixes, corrlen=0.5, **compile_args):
         vvs[v] = vv, dvv
     return vvs
 
-if __name__=='__main__':
-    compile_args = dict(args.__dict__)
-    full_prefix = compile_args.pop('prefix')
-    helpy.save_log_entry(full_prefix, 'argv')
-    dirname, prefix = os.path.split(full_prefix)
-    if '*' in full_prefix or '?' in full_prefix:
-        from glob import iglob
-        suf = '_TRACKS.npz'
-        prefixes = [ s[:-len(suf)] for s in iglob(full_prefix+suf) ]
-    elif args.sets:
-        prefixes = ["{}{i}_d/{}{i}".format(full_prefix, prefix, i=i+1)
-                    for i in xrange(args.sets)]
+
+if __name__ == '__main__':
+    helpy.save_log_entry(args.prefix, 'argv')
+    suf = '_TRACKS.npz'
+    if '*' in args.prefix or '?' in args.prefix:
+        fs = iglob(args.prefix+suf)
     else:
-        from glob import iglob
-        depth = 1
-        dirm = (dirname or '*') + (prefix + '*/')*depth
+        dirname, prefix = os.path.split(args.prefix)
+        dirm = (dirname or '*') + (prefix + '*/')
         basm = prefix.strip('/._')
-        endm = '*_TRACKS.npz'
-        prefixes = [p[:1-len(endm)]
-                    for p in iglob(dirm*depth + basm + endm)] or full_prefix
+        fs = iglob(dirm + basm + '*' + suf)
+    prefixes = [s[:-len(suf)] for s in fs] or args.prefix
+
+    helpy.save_log_entry(args.prefix, 'argv')
+    meta = helpy.load_meta(args.prefix)
     if args.verbose:
         print 'using'
         print '\n'.join([prefixes] if np.isscalar(prefixes) else prefixes)
+    helpy.sync_args_meta(args, meta, ['side', 'fps'],
+                         ['sidelength', 'fps'], [1, 1])
+    compile_args = dict(args.__dict__)
 
     label = {'o': r'$\xi$', 'par': r'$v_\parallel$', 'perp': r'$v_\perp$',
              'etapar': r'$\eta_\parallel$'}
     ls = {'o': '-', 'par': '-.', 'perp': ':', 'etapar': '--'}
     cs = {'mean': 'r', 'var': 'g', 'stderr': 'b'}
-    if args.width < 0:
-        widths = np.arange(0, 1.5, -args.width) - args.width
-        widths = np.append(widths, [1.5, 2, 2.5, 3])
-        stats = compile_widths(widths, prefixes, **compile_args)
-        plot_widths(widths, stats, normalize=args.normalize)
+    if len(args.width) > 1:
+        stats = compile_widths(prefixes, **compile_args)
+        plot_widths(args.width, stats, normalize=args.normalize)
     elif args.autocorr:
         vvs = vv_autocorr(prefixes, corrlen=10*args.fps, **compile_args)
-        plt.figure()
+        fig, ax = plt.figure()
         for v in vvs:
             vv, dvv = vvs[v]
             t = np.arange(len(vv))/args.fps
-            plt.errorbar(t, vv , yerr=dvv, label=label[v], ls=ls[v])
-        plt.title(r"Velocity Autocorrelation $\langle v(t) v(0) \rangle$")
-        plt.legend(loc='best')
+            ax.errorbar(t, vv, yerr=dvv, label=label[v], ls=ls[v])
+        ax.set_title(r"Velocity Autocorrelation $\langle v(t) v(0) \rangle$")
+        ax.legend(loc='best')
     else:
         vs = defaultdict(list)
         trackcount = compile_noise(prefixes, vs, **compile_args)
@@ -257,7 +268,8 @@ if __name__=='__main__':
             ax, ax2 = plot_hist(vs['par'], nax, axi, log=args.log,
                                 label='v_\parallel', bins=bins)
             plot_hist(vs['perp'], nax, (ax, ax2), log=args.log, label='v_\perp',
-                      bins=bins, title='Parallel & Transverse', subtitle=subtitle)
+                      bins=bins, title='Parallel & Transverse',
+                      subtitle=subtitle)
             axi += 1
             if args.subtract:
                 plot_hist(np.concatenate([vs['etapar'], vs['perp']]), nax, axi,
@@ -266,8 +278,8 @@ if __name__=='__main__':
                 axi += 1
 
     if args.save:
-        savename = os.path.abspath(full_prefix.rstrip('/._?*'))
-        savename += '_'+ args.save + '.pdf'
+        savename = os.path.abspath(args.prefix.rstrip('/._?*'))
+        savename += '_' + args.save + '.pdf'
         print 'Saving plot to {}'.format(savename)
         plt.savefig(savename)
     else:
